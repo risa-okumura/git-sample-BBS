d3f02f7523adbfe4252d87ccd9bfe8bd56d0d19d
okumura <risa.okumura@rakus.co.jp>
2018-11-19 15:29:09 +0900

    記事とコメントのフォームクラスを作成.

---
+++ src/main/java/jp/co/rakus/controller/JoinedArticleForm.java
@@ -0,0 +1,44 @@
+package jp.co.rakus.controller;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * 記事情報のフォーム.
+ * 
+ * @author risa.okumura
+ *
+ */
+public class JoinedArticleForm {
+	
+	/** 投稿ID. */
+	private int id;
+	/** 投稿者名. */
+	@NotNull(message = "値を入力してください")
+	@Size(min = 1, max = 127, message = "1桁以上127桁以下で入力してください")
+	private String name;
+	/** 投稿内容. */
+	@NotNull(message = "値を入力してください")
+	@Size(min = 1, message = "値を入力してください")
+	private String content;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
